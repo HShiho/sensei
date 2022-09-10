9f30e76a4cc58080a745567589006a47841924ff
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-10 08:11:44 +0000

    [create]deviseインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # 管理者用　URL/admin/sign_in...
+  devise_for :admin, skip:[:registrations, :passwords],controllers:{
+    sessions:"admin/sessions"
+  }
+  # 会員用　URL/public/sign_in...
+  devise_for :users, skip:[:passwords],controllers:{
+    registrations:"public/registrations",
+    sessions:'public/sessions'
+  }
+
+  namespace :admin do
+  end
+
+  namespace :public do
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
