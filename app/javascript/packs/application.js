02cb79eccaeb9a718c71282957e5e26f184b9169
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-10 14:24:49 +0000

    [add]会員ログイン画面

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,24 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// コメントアウト
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 // add
 import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip:[:registrations, :passwords],controllers:{
     sessions:"admin/sessions"
   }
-  # 会員用　URL/public/sign_in...
+  # 会員用　URL/users/sign_in...
   devise_for :users, skip:[:passwords],controllers:{
     registrations:"public/registrations",
     sessions:'public/sessions'
