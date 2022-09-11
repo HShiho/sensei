77fac935e3e9948b3ac5f898c30dd7e9ca746bbd
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-11 11:46:13 +0000

    [add]新規登録画面

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,25 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    # public_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    # public_root_path(resource)
+  end
+
+  # 会員論理削除、退会後は同じアカウントでサインインできないようにする
+  def reject_customer
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == false)
+        redirect_to new_user_session_path
+      else
+      end
+    end
+  end
 
   # GET /resource/sign_in
   # def new
