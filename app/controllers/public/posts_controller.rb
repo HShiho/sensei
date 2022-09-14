b01e4d88a53bdd7d8ddcdebbf125e01c47ccb525
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-14 09:54:46 +0000

    [add]新規投稿機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,7 @@
 class Public::PostsController < ApplicationController
   before_action :set_user
 
+
   def index
   end
 
@@ -11,10 +12,11 @@ class Public::PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    if @post.save
-      redirect_to public_user_posts_path(@post.user_id)
+    @new_post = Post.new(post_params)
+    @new_post.user_id = current_user.id
+    if @new_post.save
+      redirect_to public_root_path
+      # redirect_to public_user_posts_path(@new_post.user_id)
     else
       render public_root_path
     end
@@ -28,7 +30,15 @@ class Public::PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:user_id, :post_tag_id, :achivement, :body, :tomorrow_objective, :is_released)
+    params.require(:post).permit(:user_id, :post_tag_id, :achivement, :body, :tomorrow_objective, :is_released, :image)
+  end
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,16 @@
 class Post < ApplicationRecord
-  
+
   has_one_attached :image
-  
+
   has_many :post_comments,  dependent: :destroy
   has_many :post_tags,  dependent: :destroy
   has_many :favorites,  dependent: :destroy
   belongs_to :user
-  
-  enum achivement_status: {
+
+  enum achivement: {
     excellence: 0,
     good: 1,
     almost: 2
   }
-  
+
 end
