c39c141bf6b9a35b790e5bf8f79b3e013c70d5d9
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-16 16:46:45 +0000

    [add]いいね取り消し機能

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -16,6 +16,17 @@ class Public::FavoritesController < ApplicationController
   end
 
   def delete
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
+    # if  params[:button].to_i == 1
+      # favorite.type = "nice"
+    # elsif params[:button].to_i == 2
+      # favorite.type = "like"
+    # else
+      # favorite.type = "fight"
+    # end
+    favorite.destroy
+      redirect_to public_post_path(@post)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
     get '/about' => 'homes#about',as: 'about'
     get '/post/:id/index' => 'posts#user_index',as: 'user_posts'
     post '/post/:id/favorite' => 'favorites#create',as: 'favorite'
+    delete '/post/:id/favorite' => 'favorites#delete'
     resources :posts,except: [:update, :edit]
-      # resource :favorites, only: [:create, :destroy]
     resources :users,except: [:new, :create]
     root :to => 'homes#top'
   end
