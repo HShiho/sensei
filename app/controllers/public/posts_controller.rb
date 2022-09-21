c0beefca464655886e473441e7186836159b84ac
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-21 11:03:01 +0000

    [add]タグ検索ユーザー投稿一覧追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -5,21 +5,35 @@ class Public::PostsController < ApplicationController
     @posts = Post.all.order("created_at DESC")
     @posts_released = @posts.where(is_released: true)
     if params[:tag_ids]
-      @posts_released = []
+      @posts = []
       params[:tag_ids].each do |key, value|
         if value == "1"
           tag_posts = Tag.find_by(name: key).posts
-          @posts_released = @posts.empty? ? tag_posts : @posts & tag_posts
+          @posts = @posts.empty? ? tag_posts : @posts & tag_posts
+          @posts = @posts.order("created_at DESC")
+          @posts_released = @posts.where(is_released: true)
         end
       end
     end
   end
 
   def user_index
-    @post_all = Post.all.order("created_at DESC")
-    @posts = @post_all.where(user_id: "#{params[:id]}")
-    @post = Post.where(user_id: "#{params[:id]}").first
+    @posts = Post.where(user_id: "#{params[:id]}")
+    @posts = @posts.order("created_at DESC")
     @posts_released = @posts.where(is_released: true)
+    if params[:tag_ids]
+      @posts = []
+      params[:tag_ids].each do |key, value|
+        if value == "1"
+          tag_posts = Tag.find_by(name: key).posts
+          @posts = @posts.empty? ? tag_posts : @posts & tag_posts
+          @posts = @posts.where(user_id: "#{params[:id]}")
+          @posts = @posts.order("created_at DESC")
+          @posts_released = @posts.where(is_released: true)
+        end
+      end
+    end
+    @post = Post.where(user_id: "#{params[:id]}").first #サイドバー用
   end
 
   def destroy
