d975709649d9fa0c89d0a81d8eea796bb591163a
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-22 10:45:48 +0000

    [create]adminユーザーindex show edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,31 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to public_user_path(@user)
+    else
+      @user = User.find(@user)
+      render :edit
+    end
+  end
+
+
+   private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :is_deleted, :image )
+  end
+
 end
