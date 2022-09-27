56f1a6b613d2685a1dc7dd6bc3a983b0d3c2efbd
HShiho <108495008+HShiho@users.noreply.github.com>
2022-09-27 02:02:49 +0000

    [add]メール送信設定

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,8 +1,8 @@
 class InquiryMailer < ApplicationMailer
-  
+
   def send_mail(inquiry)
     @inquiry = inquiry
-    # mail to: ENV['TOMAIL'], subject: ' 【お問い合わせ】 ' + @inquiry.subject_i18n
+    mail to: ENV['TOMAIL'], subject: ' 【お問い合わせ】 ' + @inquiry.subject_i18n
   end
-  
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,14 +83,14 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.rails_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
     :user_name => ENV['MAIL_ADDRESS'],
     :password => ENV['MAIL_PASSWORD'],
-    # :authentication => :plain,
+    :authentication => :plain,
     :enable_starttls_auto => true
   }
 
