37e1523d1996ebfdb0b23143e002424eff181e7c
HShiho <108495008+HShiho@users.noreply.github.com>
2022-10-21 18:48:34 +0000

    [add]Rspec post テストコード

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,49 +8,64 @@ describe 'モデルのテスト' do
   end
 end
 
-describe：投稿のテスト
-  let!：let!による事前評価
-  describe：トップ画面(top_path)のテスト
-    before：トップ画面への遷移
-    context：表示の確認
-      it：トップ画面(top_path)に「こんにちは」が表示されているか
-        テストコード
-      it：public_root_pathが"/public/"であるか
-        テストコード
+describe '投稿のテスト' do
+  let!(:post) { create(:post, body:'hoge') }
+  describe 'トップ画面(public_root_path)のテスト' do
+    before do
+      visit public_root_path
+    end
+    context '表示の確認' do
+      it 'トップ画面(public_root_path)に「こんにちは」が表示されているか' do
+        expect(page).to have_content 'こんにちは'
+      end
+      it 'public_root_pathが"/public/"であるか' do
+        expect(page).to eq('/public/')
+      end
+    end
+  end
+end
 
-  describe ：投稿画面のテスト
-    context：表示の確認 
-      it：投稿ボタンが表示されているか
-        テストコード
-    context：投稿処理のテスト
-      it：投稿後のリダイレクト先は正しいか
-        テストコード
+describe '投稿画面のテスト' do
+  context '表示の確認' do
+    it '投稿ボタンが表示されているか' do
+      expect(page).to have_button '投稿'
+    end
+  end
+  context '投稿処理のテスト' do
+    it '投稿後のリダイレクト先は正しいか' do
+      fill_in FactoryBot.build(:post)
+      click_button '投稿'
+      expect(page).to have_current_path public_user_posts_path
+    end
+  end
+end
 
-  describe：一覧画面のテスト
-    before：一覧画面への遷移
-    context ：一覧の表示とリンクの確認
-      it：一覧表示画面に投稿されたもの表示されているか
-        テストコード
+describe '一覧画面のテスト' do
+  before do
+    visit public_posts_path
+  end
+  context '一覧の表示の確認' do
+    it '一覧表示画面に投稿されたもの表示されているか' do
+      expect(page).to have_content post.user.nickname
+      expect(page).to have_content post.body
+      expect(page).to have_content post.achivement
+    end
+  end
+end
 
-  describe：詳細画面のテスト
-    before：詳細画面への遷移
-    context：表示のテスト
-      it：削除リンクが存在しているか
-        テストコード
-      it：編集リンクが存在しているか
-    context：リンクの遷移先の確認
-      it：編集の遷移先は編集画面か
-        テストコード
-    context：post削除のテスト
-      it：postの削除
-        テストコード 
-        
-  describe：編集画面のテスト
-    before：編集画面への遷移
-    context：表示の確認
-      it：編集前のタイトルと本文がフォームに表示(セット)されている
-        テストコード
-      it：保存ボタンが表示される
-    context：更新処理に関するテスト
-      it：更新後のリダイレクト先は正しいか
-        テストコード
+describe '詳細画面のテスト' do
+  before do
+    visit public_post_path
+  end
+  context '表示のテスト' do
+    it '削除リンクが存在しているか' do
+      expect(page).to have_button '削除'
+      expect(page).to have_link "", href: public_post_path
+    end
+  end
+  context 'post削除のテスト' do
+    it 'postの削除' do
+      expect{ post.destroy }.to change{ Post.count }.by(-1)
+    end
+  end
+end
