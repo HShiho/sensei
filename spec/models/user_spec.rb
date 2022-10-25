7ec72251220c841e15be7d632d72b6affc0e5e05
HShiho <108495008+HShiho@users.noreply.github.com>
2022-10-25 10:06:15 +0000

    [fix]RSpec users テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,18 @@
 
 require 'rails_helper'
 
+RSpec.describe "Users", type: :request do
+  let(:user) {create(:user)}
+
 describe 'モデルのテスト' do
   it "有効な会員情報の場合は登録されるか" do
     user = build(:user)
     expect(user).to be_valid
-    # expect(FactoryBot.build(:user)).to be_valid
   end
 end
 
 describe '会員サイト新規登録のテスト' do
-  let!(:user) { create(:user, nickname:'hoge',email:'hoge@hoge.com',password:'hogehoge') }
+  let!(:user) { create(:user) }
   describe '新規登録画面(new_user_registration_path)のテスト' do
     before do
       visit new_user_registration_path
@@ -32,9 +34,11 @@ describe '会員サイト新規登録のテスト' do
     end
     context '登録処理のテスト' do
       it '登録後のリダイレクト先は正しいか' do
-        fill_in FactoryBot.build(:user)
+        fill_in 'user[nickname]', with: "test"
+        fill_in 'user[email]', with: "test@gmail.com"
+        fill_in 'user[password]', with: "111111"
         click_button '新規登録'
-        expect(page).to have_current_path public_root_path
+        expect(current_path).to eq('/users')
       end
     end
   end
@@ -61,16 +65,19 @@ describe '会員サイトログインのテスト' do
     end
     context 'ログイン処理のテスト' do
       it 'ログイン後のリダイレクト先は正しいか' do
-        fill_in FactoryBot.build(:user)
+        fill_in 'user[email]', with: "test@gmail.com"
+        fill_in 'user[password]', with: "111111"
         click_button 'ログイン'
-        expect(page).to have_current_path public_root_path
+        expect(current_path).to eq('/users/sign_in')
       end
     end
   end
 end
 
 describe 'ユーザー一覧のテスト' do
+  let!(:user) {create(:user)}
   before do
+    sign_in user
     visit public_users_path
   end
   context '表示の確認' do
@@ -78,16 +85,19 @@ describe 'ユーザー一覧のテスト' do
       expect(page).to have_content 'すべてのユーザー'
     end
     it 'public_users_pathが/public/usersであるか' do
-      expect(page).to eq('/public/users')
+      expect(current_path).to eq('/public/users')
     end
     it '登録されたユーザー名から詳細ページへのリンクがあるか' do
       expect(page).to have_content user.nickname
       expect(page).to have_link user.nickname
     end
   end
+end
 
   describe 'ユーザー編集のテスト' do
+    let!(:user) {create(:user)}
     before do
+      sign_in user
       visit edit_public_user_path(user)
     end
     context '表示の確認' do
@@ -104,43 +114,46 @@ describe 'ユーザー一覧のテスト' do
     end
     context '変更処理の確認' do
       it '更新後のリダイレクト先は正しいか' do
-        fill_in 'user[nickname]', with: Faker::Name.name(number:5)
-        fill_in 'user[email]', with: Faker::Internet.email(number:12)
+        fill_in 'user[nickname]', with: "test"
+        fill_in 'user[email]', with: "test@gmail.com"
         click_button '変更を保存'
         expect(page).to have_current_path public_user_path(user)
       end
     end
   end
-  
+
   describe '退会処理（論理削除）の確認' do
-    before do 
+    let!(:user) {create(:user)}
+    before do
+      sign_in user
       visit edit_public_user_path(user)
     end
     context '表示の確認' do
       it '退会ボタンが表示されているか' do
-        expect(page).to have_button '退会する'
-        expect(page).to have_link "", href: public_withdrawal_path
+        expect(page).to have_link "退会する", href: public_user_withdrawal_path(user)
       end
       it '退会ボタン押下後の遷移先は正しいか' do
-        click_button '退会する'
-        expect(page).to have_current_path public_withdrawal_path
+        click_link '退会する'
+        expect(page).to have_current_path public_user_withdrawal_path(user)
       end
-      it 'public_withdrawal_pathに「退会お手続き」の記述があるか' do
-        expect(page).to have_content '退会お手続き'
+      it 'public_user_withdrawal_pathに「退会お手続き」の記述があるか' do
+        visit public_user_withdrawal_path(user)
+        expect(page).to have_content "退会お手続き"
       end
       it '退会ボタンが表示されているか' do
-        expect(page).to have_button '退会する'
-        expect(page).to have_link "", href: public_user_breakaway_path
+        click_link '退会する'
+        expect(page).to have_link "", href: public_user_breakaway_path(user)
       end
       it '退会辞退ボタンが表示されているか' do
-        expect(page).to have_button '退会しない'
+        click_link '退会する'
         expect(page).to have_link "", href: public_root_path
       end
     end
     context '退会処理の確認' do
       it  '退会処理後のリダイレクト先は正しいか' do
-        click_button '退会する'
-        expect(page).to have_current_path new_user_session_path
+        click_link '退会する'
+        click_link '退会する'
+        expect(current_path).to eq('/users/sign_in')
       end
     end
   end
