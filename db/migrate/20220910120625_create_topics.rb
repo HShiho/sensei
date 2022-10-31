b3f4dce63145903e33180260fb7f401db922418e
HShiho <108495008+HShiho@users.noreply.github.com>
2022-10-31 11:24:24 +0000

    [fix]migrate create topics

--- db/migrate/20220910120625_create_topics.rb
+++ db/migrate/20220910120625_create_topics.rb
@@ -2,7 +2,7 @@ class CreateTopics < ActiveRecord::Migration[6.1]
   def change
     create_table :topics do |t|
       t.integer :user_id, null: false
-      t.integer :topic_tag_id, null: false, default: ""
+      t.integer :topic_tag_id, null: false
       t.string :title, null: false, default: ""
 
       t.timestamps
