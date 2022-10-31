81adf15ca8b34dcb749a641ba69b64b49b6191d6
HShiho <108495008+HShiho@users.noreply.github.com>
2022-10-31 12:06:02 +0000

    [fix]gem squasher導入

--- db/migrate/20220910114253_create_posts.rb
+++ db/migrate/20220910114253_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.integer :user_id, null: false
       t.integer :post_tag_id, null: false
-      t.integer :achivement, null: false, default: "0"
+      t.integer :achivement, null: false, default: 0
       t.string :body, null: false, default: ""
       t.string :tomorrow_objective, null: false, default: ""
       t.boolean :is_released, null: false, default: true
