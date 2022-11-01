396c928980d25bb8a6317c8545fe26f68caed779
HShiho <108495008+HShiho@users.noreply.github.com>
2022-11-01 14:18:09 +0000

    [add]config production お問い合わせ機能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # お問い合わせ機能
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['MAIL_ADDRESS'],
+    :password => ENV['MAIL_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
