3c78a256d7d6b07c365bdfd8428a4b7fbd681eea
HShiho <108495008+HShiho@users.noreply.github.com>
2022-11-01 13:08:36 +0000

    [fix]fontawesome import記述場所変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,10 +35,6 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
-// require("@rails/ujs").start()
-// require("turbolinks").start()
-// require("@rails/activestorage").start()
-// require("channels")
 
 var jQuery = require('jquery')
 global.$ = global.jQuery = jQuery;
