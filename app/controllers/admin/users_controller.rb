5869d552c905574ca62cbd47968552fa2032d16f
HShiho <108495008+HShiho@users.noreply.github.com>
2022-11-02 13:38:13 +0000

    [fix]admin キーワード検索　昇順変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,7 +5,7 @@ class Admin::UsersController < ApplicationController
     if params[:search] #キーワード検索
       @users = User.where("nickname LIKE ?",'%' + params[:search] + '%').order("created_at DESC").page(params[:page])
     else
-      @users = User.all.page(params[:page])
+      @users = User.all.order("created_at DESC").page(params[:page])
     end
   end
 
