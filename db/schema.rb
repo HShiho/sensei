f4e4c9ad9cd5ec6dc65a00615aa3ccb506de0c26
HShiho <108495008+HShiho@users.noreply.github.com>
2022-11-26 07:33:06 +0000

    [fix]follow 仮

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_20_113224) do
+ActiveRecord::Schema.define(version: 2022_09_27_131405) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,13 +56,6 @@ ActiveRecord::Schema.define(version: 2022_11_20_113224) do
     t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.integer "type", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "follows", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "follower_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
