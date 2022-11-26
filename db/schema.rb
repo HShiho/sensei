52cdac32b37843e9bdb180a253cb81220a628a51
HShiho <108495008+HShiho@users.noreply.github.com>
2022-11-26 08:05:27 +0000

    [fix]Follow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_26_073708) do
+ActiveRecord::Schema.define(version: 2022_09_27_131405) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
