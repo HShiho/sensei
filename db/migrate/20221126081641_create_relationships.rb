d4fe658479af837d7b0b65ea969f07b63f16e7b0
HShiho <108495008+HShiho@users.noreply.github.com>
2023-01-04 13:36:17 +0000

    [fix]migrationfile relationships

--- db/migrate/20221126081641_create_relationships.rb
+++ db/migrate/20221126081641_create_relationships.rb
@@ -3,21 +3,6 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
     create_table :relationships do |t|
       t.integer :follower_id, null: false
       t.integer :followed_id, null: false
-
-      t.timestamps
-    end
-  end
-
-  def change
-    create_table :rfollows do |t|
-      t.integer :user_id, null: false
-      t.integer :follower_id, null: false
-      t.timestamps
-    end
-
-    drop_table :follows do |t|
-      t.integer :user_id, null: false
-      t.integer :follower_id, null: false
       t.timestamps
     end
   end
